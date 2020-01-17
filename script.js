02e15c2319f7420965d63d2deb14505626e207f4
LucaArgentieri <50273216+LucaArgentieri@users.noreply.github.com>
2020-01-17 15:42:29 +0100

    Menu Hamburger Upgrade

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-$(".menu-collapsed").click(function() {
-  $(this).toggleClass("menu-expanded");
+$('.menu-burger, .menu-items').on('click', function() {
+  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
 });
