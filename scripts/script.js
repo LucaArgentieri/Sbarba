af7ae2e2b692923232280ef4f647579870d6c9e0
Luca Argentieri <LucaArgentieri@users.noreply.github.com>
2020-01-22 15:16:22 +0100

    Update - blog

--- scripts/script.js
+++
@@ -1,4 +0,0 @@
-$('.menu-burger, .menu-items').on('click', function() {
-  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
-  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
-});
