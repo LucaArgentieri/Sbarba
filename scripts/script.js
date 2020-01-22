78f99a5019b31b471534e356858a97250d514d89
Luca Argentieri <LucaArgentieri@users.noreply.github.com>
2020-01-22 17:36:52 +0100

    Update tante cose

---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+$('.menu-burger, .menu-items').on('click', function() {
+  $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+  $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+});
