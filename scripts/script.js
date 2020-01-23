7f9dd44952dd0a8a5303c88bc174ef899e93354f
LucaArgentieri <50273216+LucaArgentieri@users.noreply.github.com>
2020-01-23 17:14:24 +0100

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,27 @@
+// Script Menu Burger (JQuery)
 $('.menu-burger, .menu-items').on('click', function() {
   $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
   $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
 });
+
+// Script Searchbar (Js)
+const body = document.querySelector('body');
+const searchBtn = document.querySelector('#search');
+const searchInput = document.querySelector('#search-input');
+let active = false;
+
+body.addEventListener('click', (e) => {
+  if(e.target.id === 'search' || e.target.id === 'search-input' || e.target.id === 'search-icon') {
+    if(!active) {
+      searchBtn.classList.add('active');
+      searchInput.classList.add('active');
+      active = true;
+    }
+  } else {
+      searchBtn.classList.remove('active');
+      searchInput.classList.remove('active');
+      searchInput.value = '';
+      active = false;
+  }
+});
+
